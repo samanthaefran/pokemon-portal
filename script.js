1e34aa176aaf2c9b0b7e37bb4f84c02e7ee0769c
Samantha Frangi <sefrangi13@gmail.com>
2021-10-29 21:58:12 -0400

    adds functions to script

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-alert('omg i can do this')
+// constants
+
+const $BASE_URL = 'https://pokeapi.co/api/v2/'
+
+// variables
+
+let pokemon;
+
+// cached element
+
+const $name = $('#name');
+const $moves = $('#moves');
+const $abilities = $('#abilities');
+const $officialArt = $('#official-artwork');
+const $form = $('form');
+const $input = $('input[type="text"]');
+const $main = $('main');
+
+// event listeners
+
+$form.on('submit', handleSubmit);
+
+// functions
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+
+
+const pokemonName = $input.val();
+
+$.ajax(`${$BASE_URL}pokemon/${pokemonName}`)
+.then(function(data){
+    pokemonData = data; 
+    console.log(pokemonData);
+    render();
+}, function(error){
+    console.log(error);
+});
+}
+
+function render() {
+    $main.html(`
+    <h3>name<h3>
+    <p id='name'>${pokemonData.name}</p>
+    <h3>moves<h3>
+    <p id='moves'>${pokemonData.moves}</p>
+    `)
+    
+}
+
