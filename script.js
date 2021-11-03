fe46e56ad6fb5eb80a9a8a157542e013aa797ae0
Samantha Frangi <sefrangi13@gmail.com>
2021-11-02 22:05:33 -0400

    adds pokemon abilities and clears input on submission

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const $BASE_URL = 'https://pokeapi.co/api/v2/'
 
 // variables
 
-let pokemon;
+let pokemon, 
+    pokemonData, 
+    pokemonMoves,
+    renderedMoves;
 
 // cached element
 
 const $name = $('#name');
 const $moves = $('#moves');
 const $abilities = $('#abilities');
-const $officialArt = $('#official-artwork');
 const $form = $('form');
 const $input = $('input[type="text"]');
 const $main = $('main');
@@ -24,27 +26,49 @@ $form.on('submit', handleSubmit);
 
 function handleSubmit(evt) {
     evt.preventDefault();
+    
+    const pokemonName = $input.val();
+    
+    resetPokemonData();
 
-
-const pokemonName = $input.val();
-
-$.ajax(`${$BASE_URL}pokemon/${pokemonName}`)
-.then(function(data){
-    pokemonData = data; 
-    console.log(pokemonData);
-    render();
-}, function(error){
-    console.log(error);
-});
-}
-
-function render() {
-    $main.html(`
-    <h3>name<h3>
-    <p id='name'>${pokemonData.name}</p>
-    <h3>moves<h3>
-    <p id='moves'>${pokemonData.moves}</p>
-    `)
+    $.ajax(`${$BASE_URL}pokemon/${pokemonName}`)
+        .then(function (data) {
+            pictureOf = data.sprites.other['official-artwork'].front_default;
+            pokemonData = data;
+            pokemonMoves = data.moves;
+            renderedMoves = pokemonMoves.map(move =>  move.move.name);
+            topMoves = renderedMoves.slice(0,4);
+            pokemonAb = data.abilities;
+            renderedAb = pokemonAb.map(ability => ability.ability.name);
+            topAb = renderedAb.slice(0,4);
+            render();
+        }, function (error) {
+            console.log(error);
+        });   
+    $input.val('');
     
 }
 
+function resetPokemonData() {
+    if($('#pokemon-info')){
+        $('#pokemon-info').remove()
+        $('#pokemon-image').remove()
+    }
+}
+
+function render() {
+    $main.append(`
+    <section id='pokemon-image'>
+        <img src='${pictureOf}' alt='${pokemonData.name}'/> 
+    </section>
+    <section id='pokemon-info'>
+        <h3>name<h3>
+        <ul id='name'>${pokemonData.name}</ul>
+        <h3>moves<h3>
+        <ul id='moves'>${topMoves}</ul>
+        <h3>abilities<h3>
+        <ul id='ability'>${topAb}</ul>
+    </section>
+    `)
+
+}
